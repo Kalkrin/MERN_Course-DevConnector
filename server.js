8e08c82648b0140b7f0ccb1a679f66384b685437
Kalkrin <craigarthurarden@gmail.com>
2021-01-14 20:19:18 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { connect } = require('mongoose');
+const connectDB = require('./config/db');
+
+const app = express();
+
+//connect database
+connectDB();
+
+// Init middleware
+app.use(express.json({extended: false}));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
